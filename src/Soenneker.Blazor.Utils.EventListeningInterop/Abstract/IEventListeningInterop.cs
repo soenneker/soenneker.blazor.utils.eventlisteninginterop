c87c826a068f8848468ee3fea49715bcf69149bb
soenneker <jake@soenneker.com>
2026-08-29 14:16:52 -0500

    Improve public API XML documentation

--- src/Soenneker.Blazor.Utils.EventListeningInterop/Abstract/IEventListeningInterop.cs
+++ src/Soenneker.Blazor.Utils.EventListeningInterop/Abstract/IEventListeningInterop.cs
@@ -13,8 +13,9 @@ public interface IEventListeningInterop
     /// </summary>
     /// <param name="functionName">The name of the function to be used within JavaScript to add the event listener</param>
     /// <param name="elementId">The ID of the HTML element to attach the event listener to.</param>
-    /// <param name="eventName">The name of the event to listen for.</param>
+    /// <param name="eventName">Name of the event to publish or subscribe to.</param>
     /// <param name="dotNetCallback">The .NET callback function or object to handle the event.</param>
     /// <param name="cancellationToken">A cancellation token that can be used to cancel the asynchronous operation.</param>
+    /// <returns>A task that completes when the event listener addition is complete.</returns>
     ValueTask AddEventListener(string functionName, string elementId, string eventName, object dotNetCallback, CancellationToken cancellationToken = default);
 }
